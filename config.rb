71d1f4716195103ffe7df37abbe149e3f0860356
matthewmcvickar <matthew@matthewmcvickar.com>
2013-06-20 16:38:07 -1000

    Converted project to Compass.

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :nested
+relative_assets = true
+color_output = false
+preferred_syntax = :sass
