75ab58c73f53278a9f1e950c02da4a2735902202
matthewmcvickar <matthew@matthewmcvickar.com>
2013-06-21 17:52:21 -1000

    Background video with auto-scroll.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 jQuery(function($) {
 
   // Size the intro logo splash to the viewport height on load.
-  $('.intro').height($(window).height());
+  $('.logo').height($(window).height());
 
   // Resize the welcome panel on viewport resize.
   $(window).on('resize orientationChanged', function() {
-    $('.intro').height($(window).height());
+    $('.logo').height($(window).height());
   });
 
   // Once the page has finished loading, animate the appearance of the logo and navigation.
   $(window).load(function () {
-    $('.logo').addClass('active');
+    $('.logo img').addClass('active');
     $('nav').addClass('active');
   });
 
   // Smooth scrolling for local links.
-  $('a').smoothScroll({offset: -30});
+  $('a').localScroll({offset: -30});
 
   // Effects on scroll.
   function set_homepage_waypoint(target_container, target, offset) {
@@ -27,5 +27,39 @@ jQuery(function($) {
   }
 
   set_homepage_waypoint('.popcorn_users', '.popcorn_users', 500);
+
+  // Homepage video.
+  var video, cues;
+  video = new Popcorn('#video');
+
+  // Once the video is playable...
+  video.on('canplaythrough', function(){
+
+    // Show it, mute it, and play it.
+    $('.video_container').addClass('ready');
+    video.mute();
+    video.play();
+
+    // Scroll to sections at times.
+    cues = {
+      1 : { 'time' : '00:04', 'target' : '.intro' },
+      2 : { 'time' : '00:08', 'target' : '.popcorn_users' },
+      3 : { 'time' : '00:12', 'target' : '.examples' }
+    };
+
+    $.each(cues, function(cue, parameters) {
+      console.log(parameters.time);
+      console.log(parameters.target);
+      video.code({
+        start:   parameters.time,
+        onStart: function() {
+          console.log('yay!');
+          $.scrollTo(parameters.target, 1000, {
+            offset: -95
+          });
+        }
+      });
+    });
+  });
 
 });
